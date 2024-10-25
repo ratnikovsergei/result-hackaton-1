8b69846df9015cf0b1d176fa314f34e648629e97
Sergei Ratnikov <sergei.ratnikoff@yandex.ru>
2024-10-25 20:02:41 +0300

    start creating contextmenu, import contextmenu in app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
-import './styles.css'
+import './styles.css';
+import {ContextMenu} from "./menu";
 
+const menu = new ContextMenu('.menu');
+
+document.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+  menu.open();
+});
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,27 @@
-import {Menu} from './core/menu'
+import {Menu} from './core/menu';
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+    this.modules = [];
+  }
 
+  open() {
+    this.el.classList.add('open');
+    this.el.innerHTML = this.modules.map(module => module.toHTML()).join('');
+
+    // здесь будет обработчик событий на добавленные элементы (модули)
+    // при клике на них будет вызываться метод trigger() от каждого модуля
+    this.el.addEventListener('click', (e) => {
+
+    });
+  }
+
+  close() {
+    this.el.classList.remove('open');
+  }
+
+  add(module) {
+    this.modules.push(module);
+  }
 }
